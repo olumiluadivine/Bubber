05d077971b975b39a0a0b82326b0a4a89810142e
Divine Favour <favourme2580@gmail.com>
2022-08-08 10:17:33 +0100

    Api Controller Added

---
+++ Bubber/Controllers/BreakfastController.cs
@@ -0,0 +1,35 @@
+﻿using Bubber.Contracts.BreakFast;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bubber.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreakfastController : ControllerBase
+    {
+        [HttpPost("/breakfast")]
+        public IActionResult CreateBreakfast(CreateBreakfastRequest createBreakfast)
+        {
+            return Ok(createBreakfast);
+        }
+
+        [HttpGet("/breakfast/{id:guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("/breakfast/{id:guid}")]
+        public IActionResult UpsertBreakfast(Guid id, CreateBreakfastRequest createBreakfast)
+        {
+            return Ok(createBreakfast);
+        }
+
+        [HttpDelete("/breakfast/{id:guid}")]
+        public IActionResult DeleteBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+    }
+}
